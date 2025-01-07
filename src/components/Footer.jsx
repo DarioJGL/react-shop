f07e8be4d5c08fa44487b78316c90dcceb4265dd
DarioJGL <dariojgl0211@gmail.com>
2025-01-06 22:39:01 -0500

    Agregar configuración inicial para un proyecto de tienda en línea con React y Vue

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+export function Footer ({ filters }) {
+  return (
+    <footer className='footer'>
+
+      <p>
+        <span>⚛️</span> React Shop
+      </p>
+
+    </footer>
+
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { Filters } from './Filters'
+
+export function Header () {
+  return (
+    <header className='header'>
+      <h1>My Shop 🛒 </h1>
+      <Filters />
+    </header>
+  )
+}
